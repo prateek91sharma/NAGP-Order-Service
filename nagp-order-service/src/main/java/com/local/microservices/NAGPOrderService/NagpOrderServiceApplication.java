c43bcb07b5e5a28619a9ef548d781a5bfaa2b5c5
Prateek Sharma <prateek.sharma02@nagarro.com>
2020-07-09 13:19:03 +0530

    Initial Commit

---
+++ nagp-order-service/src/main/java/com/local/microservices/NAGPOrderService/NagpOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.local.microservices.NAGPOrderService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagpOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NagpOrderServiceApplication.class, args);
+	}
+
+}
---
+++ nagp-order-service/src/test/java/com/local/microservices/NAGPOrderService/NagpOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local.microservices.NAGPOrderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagpOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
